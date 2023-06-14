0efc00360ab85d92439e1ef383bb34516ff6b42d
Je Yun <alwaysblue21@gmail.com>
2023-06-14 09:45:18 -0400

    hungry for more

--- gitpub/server.js
+++ gitpub/server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const morgan = require("morgan") 
 const PORT = process.env.PORT
 const app = express() 
-const drinks = require("./models/drinks")
+const drinks = require("./models/drinks");
+const foods = require("./models/foods");
 
 
 app.use(morgan("dev"))
@@ -16,13 +17,21 @@ app.get("/drinks/", (req, res) => {
         arr[0] = arr[0].toUpperCase()
         return arr.join("")
     }
-    res.render("index.ejs", {capitalize, drinks})
+    res.render("index.ejs", {capitalize, drinks, foods})
 })
 
 app.get("/drinks/:id", (req, res) => {
     const id = req.params.id
     const drink = drinks[id]
-    res.render("show.ejs", {drink, id})
+    const food = foods[id]
+    res.render("show.ejs", {drink, food, id})
+})
+
+app.get("/foods/:id", (req, res) => {
+    const id = req.params.id
+    const drink = drinks[id]
+    const food = foods[id]
+    res.render("foods.ejs", {drink, food, id})
 })
 
 // LISTENER
